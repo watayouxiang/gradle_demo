3831c76dcadf35f9e58d5ebbee5164818ba6f783
TaoWang <watayouxiang@qq.com>
2021-11-12 15:27:17 +0800

    empty

---
+++ gradle_router/app/src/main/java/com/watayouxiang/gradlerouter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.watayouxiang.gradlerouter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
