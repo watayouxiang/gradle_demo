769fca63a18249162ec96d8b046482b6de6bd2b6
TaoWang <watayouxiang@qq.com>
2021-11-15 10:48:58 +0800

    empty

---
+++ gradle_router/buildSrc/src/main/groovy/com/watayouxiang/router/gradle/RouterMappingByteCodeBuilder.groovy
@@ -0,0 +1,107 @@
+package com.watayouxiang.router.gradle
+
+import jdk.internal.org.objectweb.asm.ClassWriter
+import jdk.internal.org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes
+
+/*
+// 将多个 RouterMapping_xxx 合并汇总成 RouterMapping
+public class RouterMapping {
+    public static Map<String, String> get() {
+        Map<String, String> map = new HashMap<>();
+        map.putAll(RouterMapping_1636803627725.get());
+        map.putAll(RouterMapping_6434692469264.get());
+        //...
+        return map;
+    }
+}
+
+// 通过字节码插桩技术，在.class打包成.dex文件前对其进行修改。
+// 修改字节码，通过ASM技术
+1）安装 ASM Bytecode Viewer Support Kotlin 插件，帮助写ASM代码
+2）在RouterMapping文件中，右键选择 ASM Bytecode Viewer 就能查看RouterMapping的二进制代码，
+    再点击 ASMMified 选项卡，就能查看RouterMapping的ASM代码
+ */
+class RouterMappingByteCodeBuilder implements Opcodes {
+
+    public static final String CLASS_NAME = "com/watayouxiang/app/mapping/RouterMapping"
+
+    static byte[] get(Set<String> allMappingNames) {
+        // 1、创建一个类
+        // 2、创建构造方法
+        // 3、创建get方法
+        //      1）创建一个Map
+        //      2）塞入所有映射表的内容
+        //      3）返回map
+
+        // ClassWriter.COMPUTE_MAXS 自动计算局部变量需要的栈针大小
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS)
+        // 1、创建一个类
+        classWriter.visit(V1_8,
+                ACC_PUBLIC | ACC_SUPER,
+                CLASS_NAME, // 类名
+                null,
+                "java/lang/Object",// 父类
+                null
+        )
+
+        MethodVisitor methodVisitor
+
+        // 2、创建构造方法
+        methodVisitor = classWriter.visitMethod(ACC_PUBLIC,
+                "<init>",
+                "()V",
+                null,
+                null)
+        // 开启字节码的生成和访问
+        methodVisitor.visitCode()
+        methodVisitor.visitVarInsn(ALOAD, 0)
+        methodVisitor.visitMethodInsn(INVOKESPECIAL,
+                "java/lang/Object",
+                "<init>",
+                "()V",
+                false)
+        methodVisitor.visitInsn(RETURN)
+        methodVisitor.visitMaxs(1, 1)
+        methodVisitor.visitEnd()
+
+        // 3、创建get方法
+        methodVisitor = classWriter.visitMethod(ACC_PUBLIC | ACC_STATIC,
+                "get",
+                "()Ljava/util/Map;",
+                "()Ljava/util/Map<Ljava/lang/String;Ljava/lang/String;>;",
+                null)
+        methodVisitor.visitCode()
+
+        // 1）创建一个Map
+        methodVisitor.visitTypeInsn(NEW, "java/util/HashMap")
+        methodVisitor.visitInsn(DUP)
+        methodVisitor.visitMethodInsn(INVOKESPECIAL,
+                "java/util/HashMap",
+                "<init>",
+                "()V",
+                false)
+        methodVisitor.visitVarInsn(ASTORE, 0)
+        // 2）塞入所有映射表的内容
+        allMappingNames.each {
+            methodVisitor.visitVarInsn(ALOAD, 0);
+            methodVisitor.visitMethodInsn(INVOKESTATIC,
+                    "com/watayouxiang/androiddemo/mapping/$it",
+                    "get", "()Ljava/util/Map;",
+                    false)
+            methodVisitor.visitMethodInsn(INVOKEINTERFACE,
+                    "java/util/Map",
+                    "putAll",
+                    "(Ljava/util/Map;)V",
+                    true)
+        }
+        // 3）返回map
+        methodVisitor.visitVarInsn(ALOAD, 0)
+        methodVisitor.visitInsn(ARETURN)
+        methodVisitor.visitMaxs(2, 1)
+
+        classWriter.visitEnd()
+
+        return classWriter.toByteArray()
+    }
+}
---
+++ gradle_router/buildSrc/src/main/groovy/com/watayouxiang/router/gradle/RouterMappingTransform.groovy
@@ -0,0 +1,127 @@
+package com.watayouxiang.router.gradle
+
+import com.android.build.api.transform.*
+import com.android.build.gradle.internal.pipeline.TransformManager
+import com.android.utils.FileUtils
+
+import java.util.jar.JarOutputStream
+import java.util.zip.ZipEntry
+
+class RouterMappingTransform extends Transform {
+    /**
+     * 当前 Transform 的名称
+     */
+    @Override
+    String getName() {
+        return "RouterMappingTransform"
+    }
+
+    /**
+     * 返回告知编译器，当前 Transform 需要消费的输入类型
+     * 在这里是 CLASS 类型
+     */
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS
+    }
+
+    /**
+     * 告诉编译器，当前 Transform 需要收集的范围
+     */
+    @Override
+    Set<? super QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT
+    }
+
+    /**
+     * 告诉编译器，当前 Transform 是否支持增量
+     * 通常返回 false
+     */
+    @Override
+    boolean isIncremental() {
+        return false
+    }
+
+    /**
+     * 所有 class 收集好以后，会被打包传入此方法
+     */
+    @Override
+    void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
+        //super.transform(transformInvocation)
+        // 1、遍历所有的Input
+        // 2、对Input进行二次处理
+        // 3、将Input拷贝到目标目录
+
+        RouterMappingCollector collector = new RouterMappingCollector()
+
+        // 遍历所有的输入
+        // 如果 app module 有：build/intermediates/transforms/RouterMappingTransform，说明拷贝成功
+        transformInvocation.inputs.each {
+            // 把文件夹类型的输入，拷贝到目标目录
+            it.directoryInputs.each { dirInput ->
+                def destDir = transformInvocation.outputProvider.getContentLocation(
+                        dirInput.name,
+                        dirInput.contentTypes,
+                        dirInput.scopes,
+                        Format.DIRECTORY)
+                collector.collect(dirInput.file)
+                FileUtils.copyDirectory(dirInput.file, destDir)
+            }
+            // 把 JAR 类型的输入，拷贝到目标目录
+            it.jarInputs.each { jarInput ->
+                def dest = transformInvocation.outputProvider.getContentLocation(
+                        jarInput.name,
+                        jarInput.contentTypes,
+                        jarInput.scopes,
+                        Format.JAR)
+                collector.collectFromJarFile(jarInput.file)
+                FileUtils.copyFile(jarInput.file, dest)
+            }
+        }
+
+        // RouterMappingTransform all mapping class name = [RouterMapping_1617693731656, RouterMapping_1617693732873]
+        println("${getName()} all mapping class name = " + collector.mappingClassName)
+
+        //-------------------------------- 使用 RouterMappingByteCodeBuilder
+        File mappingJarFile = transformInvocation.outputProvider.getContentLocation(
+                "router_mapping",
+                getOutputTypes(),
+                getScopes(),
+                Format.JAR)
+
+        println("${getName()} mappingJarFile = $mappingJarFile")
+
+        if (mappingJarFile.getParentFile().exists()) {
+            mappingJarFile.getParentFile().mkdirs()
+        }
+        if (mappingJarFile.exists()) {
+            mappingJarFile.delete()
+        }
+
+        // 将生成的字节码写入本地文件
+        FileOutputStream fos = new FileOutputStream(mappingJarFile)
+        JarOutputStream jarOutputStream = new JarOutputStream(fos)
+        ZipEntry zipEntry = new ZipEntry(RouterMappingByteCodeBuilder.CLASS_NAME + ".class")
+        jarOutputStream.putNextEntry(zipEntry)
+        jarOutputStream.write(RouterMappingByteCodeBuilder.get(collector.mappingClassName))
+
+        jarOutputStream.closeEntry()
+        jarOutputStream.close()
+        fos.close()
+
+/*
+验证：
+$ ./gradlew clean
+$ ./gradlew :app:assembleDebug -q
+
+日志输出：RouterMappingTransform mappingJarFile = app/build/intermediates/transforms/RouterMappingTransform/debug/48.jar
+查看build目录：app/build/intermediates/transforms/RouterMappingTransform/debug/48.jar
+
+$ cd /Users/TaoWang/Desktop/gradle_demo/gradle_router/app/build/intermediates/transforms/RouterMappingTransform/debug
+// 将 48.jar 解压到 48
+$ unzip 48.jar -d 48
+
+查看解压后的 RouterMapping.class 代码，验证正确性
+ */
+    }
+}
