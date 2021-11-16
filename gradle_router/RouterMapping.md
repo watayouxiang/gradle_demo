61be8cab41b82e156cf8d3c12ae36ab005f2977b
TaoWang <watayouxiang@qq.com>
2021-11-16 14:27:46 +0800

    empty

--- gradle_router/RouterMapping.md
+++ gradle_router/RouterMapping.md
@@ -12,15 +12,3 @@
 - url: router://watayouxiang/profile
 - realPath: com.watayouxiang.gradlerouter.ProfileActivity
 
-## 应用主页
-- url: router://page-home
-- realPath: com.watayouxiang.gradlerouter.MainActivity
-
-## 用户信息
-- url: router://page-user
-- realPath: com.watayouxiang.gradlerouter.UserActivity
-
-## 个人信息
-- url: router://watayouxiang/profile
-- realPath: com.watayouxiang.gradlerouter.ProfileActivity
-
--- gradle_router/app/src/main/java/com/watayouxiang/gradlerouter/MainActivity.java
+++ gradle_router/app/src/main/java/com/watayouxiang/gradlerouter/MainActivity.java
@@ -2,8 +2,12 @@ package com.watayouxiang.gradlerouter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.watayouxiang.gradle.router.runtime.Router;
 import com.watayouxiang.router.annotations.Destination;
@@ -15,8 +19,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv_text = findViewById(R.id.tv_text);
 
-        findViewById(R.id.tv_text).setOnClickListener(new View.OnClickListener() {
+        // 测试：获取渠道信息
+        try {
+            ApplicationInfo appInfo = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            String wtChannel = appInfo.metaData.getString("WT_CHANNEL");
+            tv_text.setText("wtChannel = " + wtChannel);
+        } catch (Exception ignored) {
+        }
+
+        // 测试：页面跳转
+        tv_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Router.INSTANCE.go(MainActivity.this, "router://watayouxiang/profile?name=imooc&message=hello");
